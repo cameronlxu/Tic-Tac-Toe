400135453c569bace64710a40889733680ad8a1a
someguy13 <ryan.kwong@gmail.com>
2020-05-11 16:01:02 -0700

    update

--- src/BoardFormatter.java
+++ src/BoardFormatter.java
@@ -1,7 +1,6 @@
 import java.awt.*;
 
 public interface BoardFormatter {
-
     public Color formatColor();
     public Font formatText(String font);
 }
--- src/View.java
+++ src/View.java
@@ -2,7 +2,6 @@ import javax.swing.*;
 import java.awt.*;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
-import java.util.Arrays;
 
 public class View extends JFrame {
     private Model model;
